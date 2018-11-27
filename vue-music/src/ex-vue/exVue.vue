03f378db847015f1bd06aedfba4f29e028eccf19
wyyyyyy <301059120@qq.com>
2018-11-27 21:01:57 +0800

    2018-11-27

---
+++ vue-music/src/ex-vue/exVue.vue
@@ -0,0 +1,61 @@
+<template>
+  <span class="demo" v-show="isShow">
+    this is a span
+  </span>
+</template>
+
+<script>
+function createEmptyVNode () {
+  const node = new VNode();
+  node.text = '';
+  return node;
+}
+function createTextVNode (val) {
+  return new VNode(undefined, undefined, undefined, String(val));
+}
+function cloneVNode (node) {
+  const cloneVnode = new VNode(
+    node.tag,
+    node.data,
+    node.children,
+    node.text,
+    node.elm
+  )
+  return cloneVnode
+}
+export default {
+  data () {
+    return {
+      tag: 'span',
+      data: {
+        // 指令集合数组
+        directives: [
+          {
+            // v-show指令
+            rawName: 'v-show',
+            expression: 'isShow',
+            name: 'show',
+            value: true
+          }
+        ],
+        // 静态class
+        staticClass: 'demo'
+      },
+      text: undefined,
+      children: [
+        // 子节点是一个文本VNode节点
+        {
+          tag: undefined,
+          data: undefined,
+          text: 'this is a span',
+          children: undefined
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
